110f0a8196bbd39f4c62adafc341af0207ac3d74
vinicius-arg <j.vin7cius@gmail.com>
2025-01-24 20:47:14 +0000

    Aulas Jan17-24

--- cont_estadio.c
+++ cont_estadio.c
@@ -5,11 +5,14 @@
 
 long long publico = 0;
 
+pthread_mutex_t mutex; // Por padrão destravado
+
 void* contar_publico(void *param);
+void inc_publico();
 
 int main(int argc, char* argv[]) {
     pthread_t t1, t2, t3, t4;
-    long n = 5000000;
+    long n = 2000000;
     
     pthread_create(&t1, NULL, contar_publico, (void*) n);
     pthread_create(&t2, NULL, contar_publico, (void*) n);
@@ -30,11 +33,21 @@ void* contar_publico(void *param) {
     long n = (long) param;
     
     for (int i = 0; i < n; i++) {
-        publico++;
-        if (i % 1000 == 0) {
-            printf("%lld\n", publico);
-        }
+        inc_publico(); // Região crítica, concorrência em escrita
+        
+        // if (i % 1000 == 0) printf("%lld\n", publico);
     }
     
     return NULL;
+}
+
+void inc_publico() {
+    /* 
+     Suspende a execução de outras threads enquanto estiver processando, apenas
+     uma thread pode travar o mutex ao mesmo tempo.
+    */
+    pthread_mutex_lock(&mutex); 
+    publico++;
+    pthread_mutex_unlock(&mutex); 
+    // Destrava após processar, liberando acesso para outras threads.
 }
